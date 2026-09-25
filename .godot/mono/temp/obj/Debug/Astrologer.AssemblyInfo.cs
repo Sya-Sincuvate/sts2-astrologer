dad631c44a7a36e9ee953c6009213432bb9fcf51
Sya <abYorha@gmail.com>
2026-09-25 17:11:03 +0800

    update card Hypnagogia

--- .godot/mono/temp/obj/Debug/Astrologer.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/Astrologer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Astrologer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da9ce8fd67b4be2b25fca3b71eb13e5a31720333")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ce24f18ff435ac101020c413f3e177aaee80b27")]
 [assembly: System.Reflection.AssemblyProductAttribute("Astrologer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Astrologer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
